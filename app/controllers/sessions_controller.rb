1e88bbb963338add6eec3b53438782e3e19ac3e9
Milton Vandol Goh <100271526+Vandal19@users.noreply.github.com>
2022-09-14 14:22:44 -0700

    Add product testing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email].strip.downcase)
-    if user.authenticate_with_credentials(params[:email].strip.downcase, params[:password])
+    user = User.find_by_email(params[:email])
+
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_path
     else
