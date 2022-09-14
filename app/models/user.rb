792232dc576fc433dfaf1a1a93a1f4f014359853
Milton Vandol Goh <100271526+Vandal19@users.noreply.github.com>
2022-09-14 09:47:12 -0700

    Add product testing and refactor user auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password_confirmation, presence: true
+  validates :password, length: { minimum: 8 }
+
+  def authenticate_with_credentials(email, password)
+    if self.authenticate(password)
+      return self
+    else
+      return nil
+    end
+  end
 end
